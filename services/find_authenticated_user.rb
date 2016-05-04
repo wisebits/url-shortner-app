47f6ac5df53e94192f13abb7f728fab60ed11828
adi-botak- <adityautamawijaya@gmail.com>
2016-05-04 08:48:06 +0800

    Fixed some typos

--- services/find_authenticated_user.rb
+++ services/find_authenticated_user.rb
@@ -3,7 +3,7 @@ require 'http'
 # Returns an authenticated user, or nil
 class FindAuthenticatedUser
   # TODO: change this to our api
-  HOST = 'http://localhost:8000/api/v1'
+  HOST = 'http://wisebits-url-shortner.herokuapp.com/api/v1'
 
   def self.call(username:, password:)
     response = HTTP.get("#{HOST}/users/#{username}/authenticate",
