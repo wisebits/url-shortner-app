05f4a9f7b74554c92fc0b24d96a6ce9d9ac3d690
omarsar <ellfae@gmail.com>
2016-05-24 13:21:14 +0800

    added store auth token with client

---
+++ services/get_all_urls.rb
@@ -0,0 +1,23 @@
+require 'http'
+
+# Return all urls belonging to a  user
+class GetAllUrls
+  def self.call(current_user:, auth_token)
+    response = HTTP.auth("Bearer #{auth_token}").
+                  .get("#{ENV['API_HOST']}/users/#{current_user['id']}/urls")
+    response.code = 200 ? extract_urls(response.parse) : nil
+  end
+
+  private
+
+  def self.extract_urls(urls)
+    urls['data'].map do |url|
+      {
+        id: url['id'],
+        full_url: url['data']['full_url'],
+        title: url['data']['title'],
+        description: url['data']['description'],
+        short_url: url['data']['short_url']
+      }
+  end
+end
